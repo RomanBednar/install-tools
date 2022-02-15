dcce23d3c39da0d4393f057a00f5c9ec219aeefa
Roman Bednar <rbednar@redhat.com>
2022-02-15 14:24:51 +0100

    wip - ssh key parsing ok

--- utils/parser.go
+++ utils/parser.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
 	"os"
 	"text/template"
 )
@@ -14,12 +18,16 @@ type Config struct {
 	Username         string
 	Password         string
 	OutputDir        string
+
+	SshPublicKey string
+	PullSecret   string
 }
 
 type TemplateParser struct {
 	data              Config
 	requestedCloud    string
 	templateDir       string
+	outputFile        string
 	cloudTemplatesMap map[string]string
 }
 
@@ -29,8 +37,15 @@ func NewTemplateParser(requestedCloud string, data Config) TemplateParser {
 	templateParser.requestedCloud = requestedCloud
 	templateParser.data = data
 
+	//Flip file paths to string.
+	templateParser.data.SshPublicKey = templateParser.fileToString(data.SshPublicKeyFile)
+
 	//Base directory for templates.
 	templateParser.templateDir = "templates/"
+
+	//Output file name.
+	templateParser.outputFile = "install-config.yaml"
+
 	//Mapping from argument to file.
 	templateParser.cloudTemplatesMap = map[string]string{
 		"aws": "aws_basic.tmpl",
@@ -47,10 +62,28 @@ func (t *TemplateParser) getTemplatePath(name string) string {
 func (t *TemplateParser) getTemplateName(name string) string {
 	templateName := t.cloudTemplatesMap[name]
 	if templateName == "" {
-		panic(fmt.Errorf("Template not found for requested cloud: %v\nUse one of: %+v", name, t.cloudTemplatesMap))
+		panic(fmt.Errorf("Template not found for requested cloud: %v\nUse one of: %q", name, t.getSupportedClouds()))
 	}
 
 	return templateName
+}
+
+func (t *TemplateParser) getSupportedClouds() []string {
+	var keys []string
+	for k, _ := range t.cloudTemplatesMap {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (t *TemplateParser) fileToString(file string) string {
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(content)
+
 }
 
 func (t *TemplateParser) ParseTemplate() {
@@ -60,7 +93,15 @@ func (t *TemplateParser) ParseTemplate() {
 
 	template := template.Must(template.New(templateName).ParseFiles(templatePath))
 
-	f, err := os.OpenFile(t.data.OutputDir, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+	err := os.Mkdir(t.data.OutputDir, 0755)
+	if err != nil && !errors.Is(err, fs.ErrExist) {
+		panic(fmt.Errorf("Could not create output dir: %v Error: %v", t.data.OutputDir, err))
+	}
+
+	output := t.data.OutputDir + "/" + t.outputFile
+	//TODO: check file presence, overwrite?
+
+	f, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		panic(err)
 	}
